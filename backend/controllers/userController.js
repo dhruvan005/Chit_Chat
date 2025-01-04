219ae35b4a4971ac39940b4175f8ccc118689fed
dhruvan005 <pateldhruvan52@gmail.com>
2025-01-04 10:56:13 +0530

    feat: Enhance cookie security

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,7 +59,7 @@ export const register = async (req, res) => {
     // console.log(token);
 
     // for checking in postman pass as raw json
-    return res.status(200).cookie("token", token, { httpOnly: true }).json({
+    return res.status(200).cookie("token", token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' }).json({
       message: "User created successfully",
     });
   } catch (error) {
@@ -94,7 +94,7 @@ export const login = async (req, res) => {
 
     // console.log(token);
 
-    return res.cookie("token", token).status(200).json({
+    return res.cookie("token", token,{ httpOnly: true, secure: process.env.NODE_ENV === 'production' }).status(200).json({
       message: "User logged in successfully",
       id: user._id,
       fullName: user.fullName,
--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -3,33 +3,23 @@ import jwt from 'jsonwebtoken';
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
-        // console.log(token);
+        console.log("Token from cookies:", token); // Debugging log
         if (!token) {
             return res.status(401).json({ message: "Unauthenticated" });
         }
-        const secretKey = process.env.JWT_ACCESS_TOKEN_SECRET
-        const decoded = await jwt.verify(token, secretKey)
-        // console.log("decode" , decoded);
+        const secretKey = process.env.JWT_ACCESS_TOKEN_SECRET;
+        const decoded = await jwt.verify(token, secretKey);
+        console.log("Decoded token:", decoded); // Debugging log
 
         if (!decoded) {
-
             return res.status(401).json({ message: "Invalid Token" });
         }
         req.id = decoded._id;
-        // console.log("id" , req.id);
-        next()
-        // return res.status(200).json({
-        //     message: "User authenticated successfully",
-        //     id: decoded.userId,
-        //     email: decoded.email
-        // })
-
-
-
+        next();
     } catch (error) {
-        console.log("Error in isAuthenticated ", error);
+        console.log("Error in isAuthenticated:", error);
         return res.status(500).json({ message: "Please Login Again" });
     }
-}
+};
 
 export default isAuthenticated;

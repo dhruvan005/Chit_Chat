1ae29a9794bd940032dc08407105ef71907fe85f
dhruvan005 <pateldhruvan52@gmail.com>
2025-01-08 22:13:28 +0530

    feat: Add success message to login response in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -97,7 +97,8 @@ export const login = async (req, res) => {
 
     // console.log(token);
 
-    return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+    return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({ 
+        message : "Logged In Successfully" ,
         _id: user._id,
         username: user.username,
         fullName: user.fullName,
